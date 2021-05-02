6660a2f9b9c902228db302b83bd493e072abc69f
Giulia Giovannoli <giovannoli.giulia@gmail.com>
2021-05-02 11:44:40 +0200

    post update, delete, change likes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -6,14 +6,19 @@ const authenticatingUser = require('../middlewares/authentication')
 const { 
   create_onePost,
   list_allPosts,
-  find_onePost
+  find_onePost,
+  delete_onePost,
+  update_onePost,
+  update_likesOfPost
 } = require('../controllers/postController')
 
+router.put('/likes/:id', update_likesOfPost)
+router.put('/:id', authenticatingUser, update_onePost)
+router.delete('/:id', authenticatingUser, delete_onePost)
 router.get('/:id', find_onePost)
 router.get('/', list_allPosts)
 
 router.post('/', authenticatingUser, create_onePost)
 
-// next steps: delete and/or update post ?? !! 
 
 module.exports = router;
