3e5c8178d74370dbd26485d43b47a40f0553bf39
Giulia Giovannoli <giovannoli.giulia@gmail.com>
2021-05-04 08:59:44 +0200

    token and creating post

--- frontend/src/components/UserDashboard.js
+++ frontend/src/components/UserDashboard.js
@@ -15,7 +15,6 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
